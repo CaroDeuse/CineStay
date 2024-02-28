73ebaa1f77f535af492a7108723a94b86624749a
Nicolas Neve <nnevedm@gmail.com>
2024-02-28 15:08:58 +0100

    owner's page of all villas + quick modification

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "villas#index"
-  resources :villas, only: %i[show new create edit update] do
+  resources :villas, only: %i[show new create edit update destroy] do
     resources :bookings, only: [:create]
     collection do
       get :my_villas
