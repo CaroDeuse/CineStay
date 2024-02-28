0b72d9e9db8320dc02c9604715d3cc02eeafb3b5
geoffroy de cooman <geoffroy.decooman@gmail.com>
2024-02-28 14:57:40 +0100

    Added two validations to booking form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :villa
   belongs_to :user
+  validates :end_date, comparison: { greater_than: :start_date }
+  validates :start_date, comparison: { greater_than: Date.today }
 end
