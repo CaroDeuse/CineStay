ebdf05b7493f3670f073b0a25fcd103584494447
Caroline Deuse <carolinedeuse@gmail.com>
2024-02-28 11:14:42 +0100

    added picture in the form and destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "villas#index"
-  resources :villas, only: %i[show new create edit update]
+  resources :villas, only: %i[show new create edit update destroy]
 end
