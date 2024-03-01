333531e6be62dd1fc0d4c526045ca0f9efc0cb5a
Caroline Deuse <carolinedeuse@gmail.com>
2024-03-01 11:15:19 +0100

    in progress

--- app/javascript/controllers/booking_cost_controller.js
+++ app/javascript/controllers/booking_cost_controller.js
@@ -2,15 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static targets = ["nightsNumber", "totalPrice"]
+  static targets = ["nightsNumber", "totalPrice", "startDate"]
 
   connect() {
     console.log("Hey I'm trying to do some JS")
   }
 
   updateFields(event) {
-    console.log("I'm binding an event");
     console.log(event);
+    console.log(this.startDateTarget);
     this.nightsNumberTarget.innerText = "Bingo!";
     this.totalPriceTarget.innerText = "re-Bingo!";
     // how do i update this text? do i need values?
