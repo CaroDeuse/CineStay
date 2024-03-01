1905d3cfc1fe8c004a54d4adfd4c5ea0935ed3cf
Caroline Deuse <carolinedeuse@gmail.com>
2024-03-01 12:56:20 +0100

    managed to make it work

--- app/javascript/controllers/booking_cost_controller.js
+++ app/javascript/controllers/booking_cost_controller.js
@@ -2,40 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static targets = ["nightsNumber", "totalPrice", "startDate", "endDate"]
-
-  connect() {
-    console.log("Hey I'm trying to do some JS")
-  }
+  static targets = ["nightsNumber", "totalPrice", "startDate", "endDate", "price"]
 
   updateFields(event) {
 
+    const nightsNumber = (Date.parse(this.endDateTarget.value) - Date.parse(this.startDateTarget.value))/1000/60/60/24;
 
-    console.log("start date");
-    console.log(Date(this.startDateTarget.value));
-    console.log(typeof Date(this.startDateTarget.value));
-    console.log("end date");
-    console.log(Date(this.endDateTarget.value));
-
-    const startDate = new Date(this.startDateTarget.value)
-    const endDate = new Date(this.endDateTarget.value)
-
-
-
-    this.nightsNumberTarget.innerText = endDate - startDate;
-    // this.nightsNumberTarget.innerText = Date(this.endDateTarget.value) - Date(this.startDateTarget.value);
-    this.totalPriceTarget.innerText = 6 - 4;
+    this.nightsNumberTarget.innerText = nightsNumber;
+    this.totalPriceTarget.innerText = this.priceTarget.innerText * nightsNumber;
 
   }
 }
-
-/*
-In your show page I just added input_html before the data cuz were
-targeting the input part of the code and simple form creates a wrapper
-around the input and the label the hints ertc etc
-
-In you JS I just added .value to the this.startDateTarget so
-that we can see the value added in the form, I advise you to do
-the flatpicker first so that your inputs are just one text field
-and easier than now were you have 3 different ones and will have
-to join three diferents values just to have a date  */
